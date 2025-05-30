d402021a4447233c5578bf5e15d7d5b333ec8f6f
isabella monsalles <153386131+IsaMon2@users.noreply.github.com>
2025-05-30 19:30:19 +0000

    api_contatos

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const conectarbanco = require('./config/db');
+const contatoRoutes = require('./routes/contatoRoutes');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use('/api/contatos', contatoRoutes);
+
+conectarbanco();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> console.log(`Servidor rodando na porta ${PORT}`));
