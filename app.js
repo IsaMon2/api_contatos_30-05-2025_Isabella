0e4583d2a1faa8c5993ef2d26e78c9db1d437535
isabella monsalles <153386131+IsaMon2@users.noreply.github.com>
2025-06-07 18:40:11 +0000

    api

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const conectarbanco = require('./config/db');
+const conectarBanco = require('./config/db');
 const contatoRoutes = require('./routes/contatoRoutes');
 
 dotenv.config();
 const app = express();
 
+// Middleware para interpretar JSON no corpo das requisições
 app.use(express.json());
+
+// Rotas principais
 app.use('/api/contatos', contatoRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API da Isabella está funcionando! 💖");
-});
-
-conectarbanco();
-
+// Conectar ao banco e iniciar o servidor
+conectarBanco();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
